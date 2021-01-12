6a4894c7955de4ffefbe990d64029bf665acd088
Bernardo Santos <bernardo.csantos@gmail.com>
2021-01-12 04:47:40 -0300

    Eager fetch author from article

--- src/api/articles.js
+++ src/api/articles.js
@@ -4,14 +4,14 @@ import Article from '../models/article.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const articles = await Article.query();
+  const articles = await Article.query().withGraphFetched('author');
   res.json(articles);
 })
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
 
-  const article = await Article.query().findById(id);
+  const article = await Article.query().findById(id).withGraphFetched('author');
 
   if (article) {
     res.json(article);
